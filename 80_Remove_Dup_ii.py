f712fe4fa5eebaf5ece05a3ab58c6d5d876c9b33
asrohan5 <rohanrohu99@gmail.com>
2025-12-10 20:34:47 +0530

    80

---
+++ 80_Remove_Dup_ii.py
@@ -0,0 +1,11 @@
+class Sol:
+  def remove_dup(self, nums:List[int]) -> int:
+    if len(nums) <=2:
+      return len(nums)
+
+    k = 2
+    for i in range(2, len(nums)):
+      if nums[i] != nums[k-2]:
+        nums[k] = nums[i]
+        k+=1
+    return k
