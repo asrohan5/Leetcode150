25d6ad32dd0526b108b6c13f87fd7e9953dc8900
asrohan5 <rohanrohu99@gmail.com>
2025-12-24 22:08:14 +0530

    167

---
+++ 167_Two_Sum_II.py
@@ -0,0 +1,14 @@
+class sol:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        sol = 0
+        l,r = 0, len(numbers) - 1
+        while l<r:
+            sol = numbers[l]+numbers[r]
+            if sol==target:
+                return [l+1, r+1]
+            if sol>target:
+                r-=1
+            else:
+                l+=1
+
+        return []
