60009819837a55cbcd88d77677490b4c6042ed99
asrohan5 <rohanrohu99@gmail.com>
2025-12-25 19:08:27 +0530

    50

---
+++ 50_Power.py
@@ -0,0 +1,12 @@
+class sol:
+  def helper(self,x:float, n:int) -> int:
+    if x==o: 
+      return 0
+    if n==0: 
+      return 1
+
+    res = helper(x*x, n//2)
+    return x*res if n%2 else res
+
+  res = helper(x, abs(n))
+  return res if n>=0 else 1/res
