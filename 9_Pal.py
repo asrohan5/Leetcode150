961a3eed43c2b21ed729029c96c28030ef561092
asrohan5 <rohanrohu99@gmail.com>
2025-12-26 19:38:10 +0530

    9

---
+++ 9_Pal.py
@@ -0,0 +1,17 @@
+class sol:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0: return False
+        div = 1
+        while x>=10*div:
+            div *= 10
+        
+        while x:
+            r = x%10
+            l = x // div
+            if l!=r: return False
+
+            x = x%div
+            x = x//10
+
+            div = div//100
+        return True
