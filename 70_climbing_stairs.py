abf518144fea0ff21ffefb38a403508847f5aaf5
asrohan5 <rohanrohu99@gmail.com>
2025-12-31 22:14:45 +0530

    70

---
+++ 70_climbing_stairs.py
@@ -0,0 +1,9 @@
+class sol:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1,1
+        for i in range(n-1):
+            t = one
+            one = one + two
+            two = t
+        return one
+        
