aedd6f5ca9ec1d3c49ad006de9e6d62f45b8caf8
asrohan5 <rohanrohu99@gmail.com>
2026-01-01 19:07:03 +0530

    322

---
+++ 322_Coin_Change.py
@@ -0,0 +1,13 @@
+import sys
+class sol:
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0  
+
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if a >= coin:
+                    dp[a] = min(dp[a], 1 + dp[a - coin])
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
