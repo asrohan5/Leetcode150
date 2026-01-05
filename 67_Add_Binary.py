a12f1796e3b1b121ca069ea03f4065aaf0f4fdfa
asrohan5 <rohanrohu99@gmail.com>
2026-01-05 19:59:19 +0530

    67

---
+++ 67_Add_Binary.py
@@ -0,0 +1,23 @@
+class sol:
+    def addBinary(self, a: str, b: str) -> str:
+        res = []
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+
+            res.append(str(total % 2))
+            carry = total // 2
+        return "".join(res)[::-1]
+
+'''
+        decimal_sum = int(a, 2) + int(b, 2)
+        return bin(decimal_sum)[2:]
+'''
