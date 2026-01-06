c3e9d1da6d4240e4a690d2a35fe0a02f1452221a
asrohan5 <rohanrohu99@gmail.com>
2026-01-06 20:45:38 +0530

    228

---
+++ 228_Summary_Ranges.py
@@ -0,0 +1,18 @@
+class sol:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        start_index = 0
+        result = []
+
+        for i in range(len(nums)):
+        # Find last index for continuous series
+            if i + 1 < len(nums) and nums[i] + 1 == nums[i+1]:
+                continue
+
+            if start_index == i:
+                result.append(str(nums[start_index]))
+            else:
+                result.append(str(nums[start_index]) + "->" + str(nums[i]))
+
+            start_index = i + 1
+
+        return result
