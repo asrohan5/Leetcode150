7767ad437b71ce08a04fa964124b3e3aa63baf0c
asrohan5 <rohanrohu99@gmail.com>
2026-01-30 22:25:12 +0530

    199

---
+++ 199_Binary_Right_tree.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        res = []
+        queue = deque([root])
+        
+        while queue:
+            level_length = len(queue)
+            for i in range(level_length):
+                node = queue.popleft()
+                # If it's the last node of the current level, add it to result
+                if i == level_length - 1:
+                    res.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                    
+        return res
